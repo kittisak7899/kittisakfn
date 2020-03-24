6ef00df22210ce96bfc18d3f37b672984035dfd1
kittisak7899 <59908384+kittisak7899@users.noreply.github.com>
2020-03-24 11:01:55 +0700

    commit

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <?php echo "รหัสนิสิต-62310133"; ?><br>
 <?php echo "ชื่อ-สกุล นายกิตติศักดิ์ แก้วทอง"; ?><br>
 <iframe width="500" height="500"
-src="https://www.youtube.com/embed/RgKAFK5djSk?autoplay=1">
+src="https://www.youtube.com/watch?v=RLRP6XJMSis&fbclid=IwAR3wZ8_9mHjiP8ie4JoXg-O0rw5N9UGfu8DzNYP5k55UdP8eiAtaWCPjOJM">
 </iframe>
 </BODY>
 </HTML>

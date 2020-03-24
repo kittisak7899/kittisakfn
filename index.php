ac12b941f46d17bb2da7d67161655eee5b9dd0a9
kittisak7899 <59908384+kittisak7899@users.noreply.github.com>
2020-03-24 11:08:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,6 @@
 <H1>
 <?php echo "รหัสนิสิต-62310133"; ?><br>
 <?php echo "ชื่อ-สกุล นายกิตติศักดิ์ แก้วทอง"; ?><br>
-<iframe width="500" height="500"
-src="https://www.youtube.com/watch?v=RLRP6XJMSis&fbclid=IwAR3wZ8_9mHjiP8ie4JoXg-O0rw5N9UGfu8DzNYP5k55UdP8eiAtaWCPjOJM">
-</iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/RLRP6XJMSis" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </BODY>
 </HTML>
